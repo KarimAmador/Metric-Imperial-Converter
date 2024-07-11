a4c3008ee2cd8d9dea24e95eb995827fda823463
KarimAmador <karimalejandroAC@gmail.com>
2024-07-11 12:50:32 -0600

    feat: finish getString

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -72,6 +72,7 @@ function ConvertHandler() {
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
+    result = `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
     
     return result;
   };
