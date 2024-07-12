674d4ebd625cf138d2334124f71598f7bbb640be
KarimAmador <karimalejandroAC@gmail.com>
2024-07-12 11:44:19 -0600

    feat: add functional api/convert route

--- routes/api.js
+++ routes/api.js
@@ -7,14 +7,30 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
-  app.get('/test', (req, res) => {
-    console.log(req.query);
-    res.json({
-      answer: convertHandler.getNum(req.query.num || '1.12/.65mi'),
-      boolean: Boolean(convertHandler.getNum(req.query.num || '1.12/.65mi')),
-      returnUnit: convertHandler.getReturnUnit(convertHandler.getUnit(req.query.num)),
-      unitName: convertHandler.spellOutUnit(convertHandler.getUnit(req.query.num)),
-      conversion: convertHandler.convert(1, 'l')
-    })
+  app.get('/api/convert', (req, res) => {
+    if (!req.query.input) {
+      return res.redirect('/');
+    }
+
+    const input = req.query.input;
+
+    try {
+      let conversion = {};
+      conversion.initNum = convertHandler.getNum(input);
+      conversion.initUnit = convertHandler.getUnit(input);
+
+      let errMsg;
+      if (!conversion.initNum) errMsg = 'invalid number';
+      if (!conversion.initUnit) errMsg = errMsg ? errMsg + ' and unit' : 'invalid unit';
+      if (errMsg) throw new Error(errMsg);
+
+      conversion.returnNum = convertHandler.convert(conversion.initNum, conversion.initUnit);
+      conversion.returnUnit = convertHandler.getReturnUnit(conversion.initUnit);
+      conversion.string = convertHandler.getString(conversion.initNum, conversion.initUnit, conversion.returnNum, conversion.returnUnit)
+
+      res.json(conversion)
+    } catch(err) {
+      res.type('text').send(err.message);
+    }
   })
 };
